cd364741ba5cc90473224d44b8e6c4ec9ea15cce
arduanovdanil <rylezz@gmail.com>
2019-02-21 20:59:15 +0500

    initial project version

---
+++ action.php
@@ -0,0 +1,32 @@
+<?php
+$year = htmlspecialchars($_POST['year']);
+// echo $year;
+
+$linkToIndexPage = "
+<p>
+    <a href=\"/inputTask/index.php\">На главную</a>
+</p>";
+
+//if (preg_match("/[-]/",$year) or $year ==0) {
+   // header('Location: /index.php');
+ //   exit ("Год должен быть больше нуля! Пожалуйста, введите корректные данные! $linkToIndexPage");
+//}
+if (preg_match("/\\D/",$year) or $year ==0) {
+    // header('Location: /index.php');
+    exit ("Пожалуйста, введите корректные данные $linkToIndexPage");
+}
+
+
+function isLeap($y){
+
+    return $y % 4 == 0 && $y%100 !=0 || $y % 400 == 0;
+}
+$answer = isLeap($year);
+if ($answer == true) {
+    echo "$year год високосный";
+    echo $linkToIndexPage;
+    }
+    else {
+    echo "$year год не високосный";
+        echo $linkToIndexPage;
+}
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Проверка високосности года</title>
+    <style>
+        form {
+            width: 150px;
+            height: 80px;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            margin-top: -40px;
+            margin-left: -75px;
+        }
+
+
+
+    </style>
+
+
+
+    </head>
+<body>
+
+    <form action="action.php" method="post">
+        <p> Введите год: <input type="number" name="year" /> </p>
+        <!-- <p>Ваш возраст: <input type="text" name="age" /></p> -->
+        <p><input type="submit" value="Год високосный?" /></p>
+    </form>
+</body>
+</html>
+
+
+
+
+
+
